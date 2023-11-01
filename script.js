f561dcdae6bb872a76c47852b1800e69c6e6887f
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-01 22:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let color=document.getElementById("colorSelect");
+document.querySelector.contains = function(selector, text) {
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].textContent === text) {
+      return elements[i];
+    }
+  }
+  return null;
+};
+function remove(){
+       var elementToFind = document.querySelector.contains('option',color.value);
+    elementToFind.remove();
+
+}
